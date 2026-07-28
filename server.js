25c692c6690350b51435dca6d043236337400a00
CR Election <sazzadbinhasan776@gmail.com>
2026-07-29 05:22:45 +0600

    Add Google Sheet integration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const axios = require("fs");
 
 const app = express();
 
@@ -143,6 +144,21 @@ app.post("/vote", (req, res) => {
         VOTES_FILE,
         JSON.stringify(votes, null, 2)
     );
+// Send vote to Google Sheet
+    axios.post(
+        "https://script.google.com/macros/s/AKfycbxHTLwS7XkethAd1GpwS1kdqTCB00Nv_6slz2wF2Fi-MhqnlD6gz38QHC1XkHx1PifheQ/exec",
+    {
+        candidate: candidate,
+        time: new Date().toISOString(),
+        id: Date.now()
+    }
+    )
+    .then(() => {
+        console.log("Vote added to Google Sheet");
+    })
+    .catch((err) => {
+        console.log("Google Sheet error:", err.message);
+    });
 
 
 
